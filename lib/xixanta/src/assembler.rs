mssola/tools.nes

commit a9f50efdef4008822287911025e0f1783dcc9638

Fix label references on control statements
Signed-off-by: Miquel Sabaté Solà <mikisabate@gmail.com>

lib/xixanta/src/assembler.rs
@@ -878,6 +878,7 @@ impl Assembler {
             Some(args) => {
                 for arg in args {
                     // Evaluate the argument as a node.
+                    self.literal_mode = None;
                     let mut bundle = self.evaluate_node(arg)?;
 
                     // If there is a missmatch between the expected number of
@@ -904,7 +905,7 @@ impl Assembler {
                             _ => panic!("bad argument when evaluating arguments"),
                         }
                     }
-                    self.push_bundle(bundle, node)?;
+                    self.push_bundle(bundle, arg)?;
                 }
             }
             None => {
@@ -2518,6 +2519,51 @@ code:
         assert_eq!(bundles[2].bytes[0], 0x20);
         assert_eq!(bundles[2].bytes[1], 0x04);
         assert_eq!(bundles[2].bytes[2], 0x80);
+    }
+
+    #[test]
+    fn proc_reference_another_segment() {
+        let mut asm = Assembler::new(one_two().to_vec());
+        asm.mappings[0].segments[0].bundles = minimal_header();
+        asm.mappings[0].offset = 6;
+        let bundles = &asm
+            .assemble(
+                r#"
+.segment "ONE"
+.addr code
+
+.segment "TWO"
+nop
+code:
+    rts
+"#
+                .as_bytes(),
+            )
+            .unwrap()[0x10..]; // Ignoring HEADER
+
+        assert_eq!(bundles[0].size, 2);
+        assert_eq!(bundles[0].bytes[0], 0x03);
+        assert_eq!(bundles[0].bytes[1], 0x80);
+    }
+
+    #[test]
+    fn cannot_fit_address_in_one_byte() {
+        let mut asm = Assembler::new(one_two().to_vec());
+        assert_error_with_assembler(
+            &mut asm,
+            r#".segment "ONE"
+.byte code
+
+.segment "TWO"
+nop
+code:
+    rts
+"#,
+            "Evaluation",
+            2,
+            false,
+            "expecting an argument that fits into a byte",
+        );
     }
 
     #[test]

commit 5ddfd96f7311292e222a1bc8287eef33a8e097d8

Reset literal mode on pending nodes
Failing to do so would spill over the previous literal mode into the
evaluation of nodes pending to be crunched.

Fixes: a9f50efdef40 ("Fix label references on control statements")

Signed-off-by: Miquel Sabaté Solà <mikisabate@gmail.com>

lib/xixanta/src/assembler.rs
@@ -320,6 +320,7 @@ impl Assembler {
             self.labels_seen = pn.labels_seen;
             self.context.force_context_switch(&pn.context);
 
+            self.literal_mode = None;
             match self.evaluate_node(&pn.node) {
                 Ok(mut bundle) => {
                     let current = &self.mappings[pn.mapping].segments[pn.segment];
@@ -2548,6 +2549,7 @@ nop
 .segment "ONE"
 lala:
     rts
+    .byte $F3
 
 .segment "TWO"
 code:
@@ -2560,12 +2562,12 @@ code:
 "#
                 .as_bytes(),
             )
-            .unwrap()[0x11..]; // Ignoring HEADER + rts from ONE
+            .unwrap()[0x12..]; // Ignoring HEADER + first two ONE
 
         // "jsr code" from ONE (notice that it's intertwined!)
         assert_eq!(bundles[0].size, 3);
         assert_eq!(bundles[0].bytes[0], 0x20);
-        assert_eq!(bundles[0].bytes[1], 0x04);
+        assert_eq!(bundles[0].bytes[1], 0x05);
         assert_eq!(bundles[0].bytes[2], 0x80);
 
         // "jsr lala" from TWO
@@ -2577,7 +2579,7 @@ code:
         // "jsr code" from TWO (again, notice that it's intertwined)
         assert_eq!(bundles[2].size, 3);
         assert_eq!(bundles[2].bytes[0], 0x20);
-        assert_eq!(bundles[2].bytes[1], 0x04);
+        assert_eq!(bundles[2].bytes[1], 0x05);
         assert_eq!(bundles[2].bytes[2], 0x80);
     }
 