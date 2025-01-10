7882e4474b77dfe4036f170cfce0e40b4f10ae93
Miquel Sabaté Solà <mikisabate@gmail.com>
2025-01-10 12:49:15 +0100

    tests: Bundle end-to-end tests into a tarball
    This way I can simply compress all the examples to traverse with a
    single file, and thus no longer polluting this repository from code from
    other repositories.
    
    Signed-off-by: Miquel Sabaté Solà <mikisabate@gmail.com>

--- scripts/test-e2e.sh
+++ scripts/test-e2e.sh
@@ -2,16 +2,27 @@
 
 set -ex
 
-rm -rf tests/out/
-mkdir -p tests/out/
+pushd tests
+
+rm -rf out/
+mkdir -p out/
+
+if [ ! -d code.nes ]; then
+    tar xzvf code.nes.tar.gz
+fi
+
+popd
 
 cargo build
 
-./target/debug/nasm -c nrom -Werror -o tests/out/sprite.nes tests/src/sprite.s
-diff tests/out/sprite.nes tests/bin/sprite.nes
+./target/debug/nasm -c nrom65 -o tests/out/sprite.nes tests/code.nes/basics/sprite.s
+diff tests/out/sprite.nes tests/code.nes/out/basics/sprite.nes
 
-./target/debug/nasm -c unrom -o tests/out/chr-ram.nes tests/src/chr-ram.s
-diff tests/out/chr-ram.nes tests/bin/chr-ram.nes
+./target/debug/nasm -c unrom -o tests/out/chr-ram.nes tests/code.nes/basics/chr-ram.s
+diff tests/out/chr-ram.nes tests/code.nes/out/basics/chr-ram.nes
 
-./target/debug/nasm -c nrom -Werror -o tests/out/flicker.nes tests/src/flicker/flicker.s
-diff tests/out/flicker.nes tests/bin/flicker.nes
+./target/debug/nasm -c nrom65 -o tests/out/flicker.nes tests/code.nes/basics/flicker.s
+diff tests/out/flicker.nes tests/code.nes/out/basics/flicker.nes
+
+./target/debug/nasm -c nrom65 -o tests/out/space.nes tests/code.nes/space/src/space.s
+diff tests/out/space.nes tests/code.nes/out/space/space.nes
