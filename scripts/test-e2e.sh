e3e54cfee6ebc17281bbbf906ff9035f594313cd
Miquel Sabaté Solà <mikisabate@gmail.com>
2025-01-22 08:06:18 +0100

    tests: Add aoc2023.nes to end-to-end tests
    Signed-off-by: Miquel Sabaté Solà <mikisabate@gmail.com>

--- scripts/test-e2e.sh
+++ scripts/test-e2e.sh
@@ -8,9 +8,14 @@ mkdir -p out/
 
 rm -rf code.nes
 tar xzf code.nes.tar.gz
+
+rm -rf aoc2023.nes
+tar xzf aoc2023.nes.tar.gz
 popd
 
 cargo build
+
+# code.nes
 
 ./target/debug/nasm -c nrom -Werror -o tests/out/sprite.nes tests/code.nes/basics/sprite.s
 diff tests/out/sprite.nes tests/code.nes/out/basics/sprite.nes
@@ -23,3 +28,10 @@ diff tests/out/flicker.nes tests/code.nes/out/basics/flicker.nes
 
 ./target/debug/nasm -c nrom -Werror -o tests/out/space.nes tests/code.nes/space/src/space.s
 diff tests/out/space.nes tests/code.nes/out/space/space.nes
+
+# aoc2023.nes
+
+./target/debug/nasm -c tests/aoc2023.nes/config/nes.cfg -o tests/out/1.nes tests/aoc2023.nes/src/1.s
+./target/debug/nasm -c tests/aoc2023.nes/config/nes.cfg -o tests/out/2.nes tests/aoc2023.nes/src/2.s
+./target/debug/nasm -c tests/aoc2023.nes/config/nes.cfg -o tests/out/3.nes tests/aoc2023.nes/src/3.s
+./target/debug/nasm -c tests/aoc2023.nes/config/nes.cfg -o tests/out/4.nes tests/aoc2023.nes/src/4.s
