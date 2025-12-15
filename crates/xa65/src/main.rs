mssola/tools.nes

commit 4961b715328d43e721697457c35968618168760f

xa65: better ensure the name of the tmp directory
Signed-off-by: Miquel Sabaté Solà <mssola@mssola.com>

crates/xa65/src/main.rs
@@ -169,7 +169,7 @@ fn get_binaries(nasm_name: String) -> Result<(PathBuf, PathBuf), String> {
 fn temporary_dir() -> PathBuf {
     let tmp = &std::env::temp_dir();
     let paths = std::fs::read_dir(tmp).unwrap();
-    let name = format!("xa65-{}", paths.count());
+    let name = format!("xa65-{}", paths.count() + 1);
 
     tmp.join(name)
 }
@@ -237,7 +237,7 @@ fn main() {
     // as an intermediate step.
     let dir = temporary_dir();
     if let Err(e) = std::fs::create_dir(&dir) {
-        die(e.to_string());
+        die(format!("could not create temporary directory: {}", e));
         return;
     }
 

commit b7645c842f6c3fd718debe8de3e32ae346b17567

xa65: use the current timestamp for the tmp directory
The easy "let's count how many directories there are in /tmp" trick was
prone to errors. Hence, let's make things (hopefully) easier by just
appending the current timestamp in milliseconds to the directory
name.

Fixes: 4961b715328d ("xa65: better ensure the name of the tmp directory")
Signed-off-by: Miquel Sabaté Solà <mssola@mssola.com>

crates/xa65/src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::{Path, PathBuf};
 use std::process::Command;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 /// Version for this program.
 const VERSION: &str = "0.1.0";
@@ -168,8 +169,13 @@ fn get_binaries(nasm_name: String) -> Result<(PathBuf, PathBuf), String> {
 // Returns the path to the temporary directory that can be used for the run.
 fn temporary_dir() -> PathBuf {
     let tmp = &std::env::temp_dir();
-    let paths = std::fs::read_dir(tmp).unwrap();
-    let name = format!("xa65-{}", paths.count() + 1);
+
+    let start = SystemTime::now();
+    let epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("xa65 (error): could not get current time.");
+
+    let name = format!("xa65-{}", epoch.as_millis());
 
     tmp.join(name)
 }