7b83a222ee52c579d127ec40701c57b5576ddf71
Miquel Sabaté Solà <mssola@mssola.com>
2026-07-09 22:15:05 +0200

    scripts: separate fuzzy testing into its own file
    This way we can simply run fuzzy testing multiple times from the shell
    without having to run the full tests too.
    
    Signed-off-by: Miquel Sabaté Solà <mssola@mssola.com>

--- scripts/full-test.sh
+++ scripts/full-test.sh
@@ -9,12 +9,4 @@ cargo clippy --all-targets --all-features
 cargo test --all-targets --all-features -- --nocapture
 
 ./scripts/test-e2e.sh
-
-pushd lib/xixanta
-cargo +nightly fuzz run fuzz_target_parser -- -max_total_time=180
-cargo +nightly fuzz run fuzz_target_assembler -- -max_total_time=180
-popd
-
-pushd lib/header
-cargo +nightly fuzz run fuzz_target_header -- -max_total_time=180
-popd
+./scripts/fuzz.sh
