01bb4fee8c45caee757b3bdc5432fd88dbcdfca3
Miquel Sabaté Solà <mssola@mssola.com>
2026-09-15 18:02:43 +0200

    fuzz: change names to have dashes instead of underscore
    From the latest update of Rust's stable channel, this was raised as a
    bad format for fuzz names.
    
    Signed-off-by: Miquel Sabaté Solà <mssola@mssola.com>

--- scripts/fuzz.sh
+++ scripts/fuzz.sh
@@ -3,10 +3,10 @@
 set -ex
 
 pushd lib/xixanta
-cargo +nightly fuzz run fuzz_target_parser -- -max_total_time=180
-cargo +nightly fuzz run fuzz_target_assembler -- -max_total_time=180
+cargo +nightly fuzz run fuzz-target-parser -- -max_total_time=180
+cargo +nightly fuzz run fuzz-target-assembler -- -max_total_time=180
 popd
 
 pushd lib/header
-cargo +nightly fuzz run fuzz_target_header -- -max_total_time=180
+cargo +nightly fuzz run fuzz-target-header -- -max_total_time=180
 popd
